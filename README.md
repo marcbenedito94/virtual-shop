f7c0ef914f765d042f449d985d94df412b40a2ed
marcbenedito94 <marcbene94@gmail.com>
2019-05-20 09:41:57 +0200

    Readme added

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a very basic Virtual Shop developed in HTML, JavaScript and PHP developed by me (Marc Benedito Pons).
+
+The file 'comandos-sql.txt' contains the SQL commands to create Database for the web.
--- iniciar_prueba.ps1
+++
@@ -1,9 +0,0 @@
-docker build -t joel .
-
-docker run --name joel-cont -d -p 80:80 -it joel /bin/bash
-
-docker exec -i joel-cont service apache2 start
-
-Start-Process "http://localhost:80/joel/index.html"
-
-PAUSE
