2e4fb4fb6af356725446e7322a27946b9414a6e0
marcbenedito94 <marcbene94@gmail.com>
2019-05-27 12:16:33 +0200

    more than 2 languages to select onchange

--- components/header/header.js
+++ components/header/header.js
@@ -9,7 +9,7 @@ var header_out_content = `
         <img id="header-img" src="" alt="Header Image">
         <a id="login-link" href="./main/login/login.html">` + textHeaderLoginLink() + `</a>
         <a id="logout-link" class="` + showHideLogoutLink() + `" onclick="` + logout() + `" href="./index.html">logout</a>
-        <select id="language-selector">
+        <select id="language-selector" onchange="getSelectedLanguage()">
             <option value="es">Spanish</option>
             <option value="en">English</option>
             <option value="it">Italian</option>
