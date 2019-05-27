d944a04c8bfe13f4d5f1c6daf476466ed37a153f
marcbenedito94 <marcbene94@gmail.com>
2019-05-27 11:52:37 +0200

    more than 2 languages to select

--- components/header/header.js
+++ components/header/header.js
@@ -6,37 +6,57 @@ var login_link_text = ''; //will be 'login' or the current user
 var current_language = ''; //current language of the web
 
 var header_out_content = `
-        <img id="header-img" src="./img/header.jpg" alt="Header Image">
+        <img id="header-img" src="" alt="Header Image">
         <a id="login-link" href="./main/login/login.html">` + textHeaderLoginLink() + `</a>
         <a id="logout-link" class="` + showHideLogoutLink() + `" onclick="` + logout() + `" href="./index.html">logout</a>
+        <select id="language-selector">
+            <option value="es">Spanish</option>
+            <option value="en">English</option>
+            <option value="it">Italian</option>
+            <option value="fr">French</option>
+        </select>
         <a id="link-lang-es" class="link-lang" href="#" onclick="setLanguageToSpanish()">es</a>
         <a id="link-lang-en" class="link-lang" href="#" onclick="setLanguageToEnglish()">en</a>`;
 
-//change language of the web to Spanish
-function setLanguageToSpanish() {
-    current_language = 'es';
+function getSelectedLanguage() {
+    var selected_language = document.getElementById('language-selector').value;
+    current_language = selected_language;
+
     getLincsJsonByLang(); //function of functions.js
 
     var current_location = window.location.pathname;
 
     if (current_location == '/joel/index.html' || current_location == '/joel/index.html#') {
-        current_language = 'es';
+        current_language = selected_language;
         getInfoLincsJsonByLang();
     }
 }
 
-//change language of the web to Spanish
-function setLanguageToEnglish() {
-    current_language = 'en';
-    getLincsJsonByLang(); //function of functions.js
+// //change language of the web to Spanish
+// function setLanguageToSpanish() {
+//     current_language = 'es';
+//     getLincsJsonByLang(); //function of functions.js
 
-    var current_location = window.location.pathname;
+//     var current_location = window.location.pathname;
 
-    if (current_location == '/joel/index.html' || current_location == '/joel/index.html#') {
-        current_language = 'en';
-        getInfoLincsJsonByLang();
-    }
-}
+//     if (current_location == '/joel/index.html' || current_location == '/joel/index.html#') {
+//         current_language = 'es';
+//         getInfoLincsJsonByLang();
+//     }
+// }
+
+// //change language of the web to Spanish
+// function setLanguageToEnglish() {
+//     current_language = 'en';
+//     getLincsJsonByLang(); //function of functions.js
+
+//     var current_location = window.location.pathname;
+
+//     if (current_location == '/joel/index.html' || current_location == '/joel/index.html#') {
+//         current_language = 'en';
+//         getInfoLincsJsonByLang();
+//     }
+// }
 
 //set text of login_link
 function textHeaderLoginLink() {
